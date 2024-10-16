b2dbcd01e3e037824357ed7aca3a0f768cceb571
DanielaSabino06 <danielauninove@outlook.com>
2024-10-16 19:42:13 -0300

    atividade corrigida

--- JS/Exercicio-Casa1/main.js
+++ JS/Exercicio-Casa1/main.js
@@ -5,15 +5,15 @@ console.clear()
 console.log("Cálculo da média")
 
 nota1 = 7.5
-console.log(nota1)
+
 nota2 = 5
-console.log(nota2)
+
 nota3 = 8
-console.log(nota3)
+
 nota4 = 7
-console.log(nota4)
-//processamento
-console.log(media = (nota1 + nota2 + nota3 + nota4) / 2)
+
+//processament
+media= (nota1 + nota2 + nota3 + nota4)/ 4
 console.log(`Média: ${media.toFixed(1)}`)
 
 
