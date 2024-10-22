8094086a9e83c989c3e754486254edaae5a27183
DanielaSabino06 <danielauninove@outlook.com>
2024-10-21 21:36:39 -0300

    Aula 4 de condicionais

---
+++ JS/JS04-Condicionais/main.js
@@ -0,0 +1,38 @@
+// verificar se o Caio esta com a blusa do IOS
+
+// let caioComBlusa =false
+
+// if(caioComBlusa == true){  // se for true
+//     console.log("Caio pode assistir a aula!")
+// }else{ //se for false
+//     console.log("VOCÊ NÃO VAI ENTRAR!!!");
+// }
+
+
+console.clear()
+
+// Um jogador somente irá passar para a fase 2 se atingir 1000 pontos.
+
+let pontos = 1000
+
+if(pontos >= 1000){
+    console.log("Passou para a fase 2!")
+} //false não retorna nada
+
+
+// resto da divisão do numero 2 for 0 então é  PAR
+//resto da divisão do numero 2 for 1 então é IMPAR 
+
+let ImparPar = 17
+if(ImparPar % 2 == 0){
+    console.log("Par");
+}else{
+    console.log("Impar");
+}
+
+
+
+
+
+
+
